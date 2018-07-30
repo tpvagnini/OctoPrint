1b069981949bb0b25625e8ea7e4d6b5eb4417502
Tom <thomas@vagnini.org>
2018-07-30 18:34:29 -0400

    helloworld plugin ui modification going with tutorial

--- src/octoprint/plugins/helloworld/octoprint_helloworld/__init__.py
+++ src/octoprint/plugins/helloworld/octoprint_helloworld/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin):
     def on_after_startup(self):
             self._logger.info("Hello World!")
 
