5bccddfa428f343301ce7fd3a1e3c8638c793a6a
Tom <thomas@vagnini.org>
2018-07-30 18:12:52 -0400

    added log output and implemented the cookie cutter template

--- src/octoprint/plugins/helloworld/octoprint_helloworld/__init__.py
+++ src/octoprint/plugins/helloworld/octoprint_helloworld/__init__.py
@@ -1,6 +1,12 @@
 #coding=utf-8
 from __future__ import absolute_import
 
+import octoprint.plugin
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin):
+    def on_after_startup(self):
+            self._logger.info("Hello World!")
+
 __plugin_name__ = "Hello World"
 __plugin_version__ = "1.0.0"
 __plugin_description__ = "A quick hello world example plugin for octoprint"
