afda65b847db79ad35cb091ed9204be9172776d4
Tom <thomas@vagnini.org>
2018-07-30 14:18:12 -0400

    Added helloworld.py into the plugins folder

---
+++ src/octoprint/plugins/helloworld.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+from __future__ import absolute_import
+
+__plugin_name__ = "Hello World"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "A quick hello world example plugin for octoprint"
